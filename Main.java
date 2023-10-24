aa989a65cd4194639cfcedd1ad9f7106630280e2
Celvis <celvis.hacks@gmail.com>
2023-10-23 22:27:23 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,86 @@
+// AP COMSCI - StringShortnerAlgoritm : Main.java
+// By: Celvis
+// Version: 0.0.1
+
+/*
+ * This is the 4th assignment for ProjectStem - 10/23/2023
+ * Written By: Celvis
+ * Please note: If you are taking this for your usage and the grading doesn't appear right, try removing "import java.io.IOException" and removing "throws IOException"
+ */
+
+// Import
+import java.io.IOException;
+import java.util.*;
+
+// Main Class
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Store/Input
+        System.out.println("Type the message to be shortened");
+        String message = scan.nextLine().toLowerCase();
+
+        // Algorithm 1
+        int vowelCount = 0; // vowelCount variable
+        String shortenedMessage1 = ""; // shortened message
+        char prevChar = ' '; // previous character
+        boolean isWordStart = true; // isWordStart variable
+
+        // remove characters
+        for (char c : message.toCharArray()) {
+            if ("aeiou".contains(String.valueOf(c))) {
+                if (isWordStart) {
+                    shortenedMessage1 += c;
+                    isWordStart = false;
+                } else {
+                    vowelCount++;
+                }
+            } else {
+                if (c == ' ' || c != prevChar) {
+                    shortenedMessage1 += c;
+                }
+            }
+            if (c == ' ') {
+                isWordStart = true;
+            }
+            prevChar = c;
+        }
+
+        // Algorithm 1 output
+        System.out.println("\nAlgorithm 1");
+        System.out.println("Vowels removed: " + vowelCount);
+        System.out.println("Repeats removed: " + (message.length() - shortenedMessage1.length()));
+        System.out.println("Algorithm 1 message: " + shortenedMessage1);
+        System.out.println("Algorithm 1 characters saved: " + (message.length() - shortenedMessage1.length()) + "\n");
+
+        // Algorithm 2
+        int uniqueCharCount = 0; // uniqueCharCount variable
+        String shortenedMessage2 = "";
+
+        // count the number of unique characters
+        for (char c : message.toCharArray()) {
+            if (c != ' ' && shortenedMessage2.indexOf(c) == -1) {
+                uniqueCharCount++;
+                int charCount = 0;
+                for (char d : message.toCharArray()) {
+                    if (c == d) {
+                        charCount++;
+                    }
+                }
+                shortenedMessage2 += charCount + String.valueOf(c);
+            }
+        }
+
+        // Algorithm 2 output
+        System.out.println("Algorithm 2");
+        System.out.println("Unique characters found: " + uniqueCharCount);
+        System.out.println("Algorithm 2 message: " + shortenedMessage2);
+        System.out.println("Algorithm 2 characters saved: " + (message.length() - shortenedMessage2.length()));
+
+        // close scanner and exit program
+        scan.close();
+        System.exit(0);
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# AP COMSCI - StringShortnerAlgorithm | Main.java
+# Version: 0.0.1
+# By: Celvis
+
+## Purpose:
+
+- For my AP COMSCI class
+<br />
+
+## FAQ:
+- Have latest version of Java
+- This program is part of my AP COMSCI class
+- Please note: Please note: If you are taking this for your usage and the grading doesn't appear right, try removing "import java.io.IOException" and removing "throws IOException"
+- Also note: I am not at fault if you get in trouble for using this. I had posted this for people to learn, I left the previous note for those who are incompetent and are using this for their assignment
+- Discord: Celvis - If you have any issues, please contact me here.
+
+### How to use:
+- cd /AP COMSCI StringShortnerAlgorithm
+- javac Main.java
+
+# Languages used:
+- Java
+<img align="left" alt="" width="26px" src="https://camo.githubusercontent.com/651195b8c66a9dd22316e672992077dbcecea4ca904b45a6681558ebc0ecc517/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f656e2f7468756d622f332f33302f4a6176615f70726f6772616d6d696e675f6c616e67756167655f6c6f676f2e7376672f33303070782d4a6176615f70726f6772616d6d696e675f6c616e67756167655f6c6f676f2e7376672e706e67" style="padding-right:10px;" />
+
+<br />
+<br />
+
+---
+
+### Changelog:
+Changes:
+1. Nothing at this current time.
+
+TODO:
+1. Nothing at this current time.
